675c801225e37ab9f93aeebd948a1de68fe4d046
DumisaniLesley <44291482+DumisaniLesley@users.noreply.github.com>
2022-02-22 08:28:41 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+const humbegar = document.getElementById('humbegar');
+const navLinks = document.getElementById('navLinks');
+
+humbegar.addEventListener('click', () => {
+    navLinks.classList.toggle('show');
+});
